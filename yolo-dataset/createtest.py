b8b52e2ddbc1167484bdc01b75d0071465f0cbec
Víctor Altamirano <v.a.i@hotmail.com>
2020-02-17 22:06:53 -0600

    Update createtest.py

--- yolo-dataset/createtest.py
+++ yolo-dataset/createtest.py
@@ -1,8 +1,8 @@
 import glob, os
 os.listdir()
-os.chdir("D:/OneDrive/Documentos/GitHub/AgroJetson/yolo-dataset/img")
+os.chdir("img")
 files=[]
-f= open('D:/OneDrive/Documentos/GitHub/AgroJetson/yolo-dataset/train.txt',"w+")
+f= open('train.txt',"w+")
 for file in glob.glob("*.jpg"):
     print(str(file))
     f.write("data/img/"+str(file)+"\n")
